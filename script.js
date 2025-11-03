9e59486299aeedb50a0bc45e4efb9d65d64fe4aa
miswanto1982 <miswanto79@guru.smk.belajar.id>
2025-11-03 12:17:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ✅ URL Google Apps Script kamu — sudah benar dan siap pakai
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzVIXtjBmqBHwZTSZhujxgXFqsrOOh8R-sRbL1KHz9LeVcHf0QsX4w6HsR0loFFICkfwg/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzZmwxOOeNn70kWB7boRlGWt1cWrVvd5CYg5DyIbMryeq1HpxAnSm7aios6HitguaRyvQ/exec';
 
 // Ambil form dengan id quizForm
 const form = document.getElementById('quizForm');
@@ -28,3 +28,4 @@ form.addEventListener('submit', e => {
   // Kosongkan form setelah kirim
   form.reset();
 });
+
